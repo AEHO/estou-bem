d04d811820e1ae77fdeb4542163f68fc489049b2
Mateus Zitelli <zitellimateus@gmail.com>
2014-04-27 00:27:12 -0300

    Adiciona navbar configurada e adiciona evento watch no gulp

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+(function(){
+	var renderTemplates = function(templates){
+		$.each(templates, function(template_name, template){
+			template.html = template.template.render(template.data);
+			$(template.destination).html(template.html);
+		});
+	}
+
+	var templates = {
+		navbar: {
+			template: require('./templates/template.html'),
+			destination: '#content',
+			data:{message:'aeaeaeae'}
+		},
+		content: {
+			template: require('./templates/navbar.html'),
+			destination: '#navbar',
+			data:{}
+		}
+	};
+
+	renderTemplates(templates);
+}());

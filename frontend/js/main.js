ffc2320d7db03df824092de29388dfdfd7821577
Ciro S. Costa <ciro.costa@usp.br>
2014-04-27 04:11:34 -0300

    updates

---
+++ frontend/js/main.js
@@ -0,0 +1,63 @@
+(function(){
+    var templates = {
+        alarm: {
+            template: require('./templates/alarm.html'),
+            destination: '#alarm',
+            data:{message:''},
+        },
+        navbar: {
+            template: require('./templates/navbar.html'),
+            destination: '#navbar',
+            data:{},
+        },
+        configAlarme: {
+            template: require('./templates/config-alarme.html'),
+            destination: '#config-alarm',
+            data: {},
+        },
+        main: {
+            template: require('./templates/main.html'),
+            destination: '#main',
+            data: {},
+        }
+    };
+
+    var toggleDiv = function (div, hidden) {
+
+        if (typeof hidden === 'undefined')
+            throw new Error('coloca o hidden mano');
+
+        console.log(div);
+
+        var $div = $(div);
+        if (hidden) {
+
+            console.log('mostrar');
+
+            return $div.hide().removeClass('hidden').fadeIn();
+        }
+        console.log('tirar');
+        return $div.fadeOut();
+    };
+
+    var renderTemplates = function(templates){
+        $.each(templates, function(template_name, template){
+            template.html = template.template.render(template.data);
+            $(template.destination).html(template.html);
+        });
+    };
+    if(document.location.hash === '#' || !document.location.hash){
+        toggleDiv("#config-alarm", true);
+    }else{
+        toggleDiv(document.location.hash, true);
+    }
+
+
+    $(window).on('hashchange', function() {
+        toggleDiv(document.location.hash, true);
+    });
+
+    renderTemplates(templates);
+}());
+
+var events = require('./event.js');
